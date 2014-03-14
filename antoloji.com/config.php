148e5a598fe9f65b980a09074b6af66902772194
hasantayyar <tayyar.besik@gmail.com>
2014-03-14 20:55:32 +0200

    remove ant.

--- antoloji.com/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-require __DIR__.'../libs/Process.php';
-require __DIR__.'../libs/ganon.php';
-$base = "http://www.antoloji.com";
-$start_url = "http://www.antoloji.com/siir";
-
--- antoloji.com/run.sh
+++
@@ -1,1 +0,0 @@
-php get.php
